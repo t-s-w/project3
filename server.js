d3912d1d1d0c64338ce8fcd229e761d0ef5e9c24
t-s-w <tobby.satyarama@gmail.com>
2023-06-26 20:41:04 +0800

    feat: add express skeleton

--- server.js
+++ server.js
@@ -3,17 +3,23 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 
-require('dotenv').config();
-
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 
 // Configure both serve-favicon & static middleware
-// to serve from the production 'build' folder
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'build')));
+// to serve from the production 'dist' folder
+// app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Put API routes here, before the "catch all" route
+
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
@@ -21,11 +27,4 @@ const port = process.env.PORT || 3001;
 
 app.listen(port, function () {
     console.log(`Express app running on port ${port}`)
-});
-// Put API routes here, before the "catch all" route
-
-// The following "catch all" route (note the *) is necessary
-// to return the index.html on all non-AJAX requests
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });

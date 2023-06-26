2cdc6d2a898b7c2e4a089cc1f571491527a74e96
t-s-w <tobby.satyarama@gmail.com>
2023-06-26 20:50:51 +0800

    feat: add db connectivity

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 
+// Always require and configure near the top 
+require('dotenv').config();
+require('./config/db');
+
 const app = express();
 
 app.use(logger('dev'));

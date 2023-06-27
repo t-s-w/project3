b969df52323782a519068f523ed0249f61fc9208
t-s-w <tobby.satyarama@gmail.com>
2023-06-27 23:07:45 +0800

    finish configuring tailwind

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-
+import { Routes, Route } from 'react-router-dom'
+import SeatSelect from '../SeatSelect/SeatSelect'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>App</h1>
+    <Routes>
+      <Route path="/" element={<h1>App</h1>} />
+      <Route path="/seatselect" element={<SeatSelect />} />
+    </Routes>
   )
 }
 

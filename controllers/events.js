ebe8682cb78e93da002d72ddb4792b3d55bda219
t-s-w <tobby.satyarama@gmail.com>
2023-06-27 14:02:08 +0800

    feat: add utility script to update event db from ticketmaster

--- controllers/events.js
+++ controllers/events.js
@@ -1,9 +1,16 @@
 import Event from '../models/Event.js';
+import vars from '../config/vars.js';
 
 const eventsCtrl = {
     getAll: async (req,res) => {
         const events = await Event.find();
         res.json(events);
+    },
+    refreshData: async (req,res) => {
+        const eventData = await fetch(vars.TICKETMASTER_API).then(res => res.json())
+        eventData._embedded.events.forEach(async x => {
+            const y = await Event.replaceOne({id: x.id},x,{upsert:true})
+        })
     }
 }
 
---
+++ src/utilities/update_data.js
@@ -0,0 +1,12 @@
+import Event from '../../models/Event.js';
+import vars from '../../config/vars.js';
+import '../../config/db.js';
+
+async function refresh (req,res) {
+    const eventData = await fetch(vars.TICKETMASTER_API).then(res => res.json())
+    eventData._embedded.events.forEach(async x => {
+        const y = await Event.replaceOne({id: x.id},x,{upsert:true})
+    })
+}
+
+refresh()

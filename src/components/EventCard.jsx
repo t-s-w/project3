f3e7736ecebeae21b17e96fb8fcae2515dc4d331
ilyas182 <ilyas2104@gmail.com>
2023-06-28 20:54:57 +0800

    display img for each event

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -3,8 +3,13 @@ import { Link } from "react-router-dom";
 export default function EventCard({event}) {
     return (
     <>
+    <img 
+    src={event?.images[0].url}
+    width={200}
     
+    />
     <Link to="/events/:eventId">{event?.name}</Link>
+
     </>
     )
 }

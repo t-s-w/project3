3b4b81158d59abe70d847e2f64886bb17c4d77e6
t-s-w <tobby.satyarama@gmail.com>
2023-06-28 17:03:58 +0800

    make eventCtrl.findById return only one event

--- controllers/events.js
+++ controllers/events.js
@@ -2,20 +2,27 @@ import Event from '../models/Event.js';
 import vars from '../config/vars.js';
 
 const eventsCtrl = {
-    getAll: async (req,res) => {
+    getAll: async (req, res) => {
         const events = await Event.find();
         res.json(events);
     },
-    refreshData: async (req,res) => {
+    refreshData: async (req, res) => {
         const eventData = await fetch(vars.TICKETMASTER_API).then(res => res.json())
         eventData._embedded.events.forEach(async x => {
-            const y = await Event.replaceOne({id: x.id},x,{upsert:true})
+            const y = await Event.replaceOne({ id: x.id }, x, { upsert: true })
         })
     },
-    findById: async (req,res) => {
-        const {id} = req.params;
-        const event = await Event.find({id: id});
-        res.json(event)
+    findById: async (req, res) => {
+        const { id } = req.params;
+        try {
+            const event = await Event.find({ id: id });
+            if (event.length < 1) {
+                res.status(404).json({ "msg": "Requested event id not found" })
+            }
+            res.json(event[0])
+        } catch {
+            res.status(404).json({ "msg": "Id not found!" })
+        }
     }
 }
 

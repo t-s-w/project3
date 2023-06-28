2883bb65494883cc757091361be635dcae74c64e
t-s-w <tobby.satyarama@gmail.com>
2023-06-28 10:10:08 +0800

    feat: finish seat selector

--- src/components/SeatSelector.jsx
+++ src/components/SeatSelector.jsx
@@ -1,7 +1,10 @@
 import Seat from './Seat'
 import generate2DigitNums from '../utilities/generate-numbers'
+import { useState } from 'react'
 
 export default function SeatSelector() {
+    const [selected, setSelected] = useState({ row: undefined, start: undefined, end: undefined })
+
     const config1 = [{ row: "A", count: 10 },
     { row: "B", count: 11 },
     { row: "C", count: 12 },
@@ -13,8 +16,26 @@ export default function SeatSelector() {
     { row: "I", count: 15 },
     { row: "J", count: 15 }]
 
-    return <div className="config1 flex flex-col place-items-center">{config1.map(x => {
-        const seats = generate2DigitNums(x.count).map(num => x.row + num)
-        return <div key={x.row} className="flex flex-row place-items-center">{seats.map(x => <Seat name={x} key={x} />)}</div>
-    })}</div>
+    function handleClick(row, num) {
+        const number = parseInt(num)
+        if (row === selected.row && (selected.start - number === 1)) {
+            setSelected({ ...selected, start: number })
+        } else if (row === selected.row && (selected.end - number === -1)) {
+            setSelected({ ...selected, end: number })
+        } else {
+            setSelected({ row: row, start: number, end: number })
+        }
+    }
+
+    return <div className="grid grid-cols-3 grid-rows-2">
+        <div className="config1 w-full flex flex-col place-items-center col-span-3">{
+            config1.map(x => {
+                const seats = generate2DigitNums(x.count).map(num => ({ row: x.row, num: num }))
+                return <div key={x.row} className="flex flex-row place-items-center">{
+                    seats.map(x => <Seat row={x.row} num={x.num} key={x.row + x.num} selected={x.row === selected.row && parseInt(x.num) >= selected.start && parseInt(x.num) <= selected.end} onClick={() => handleClick(x.row, x.num)} />)
+                }</div>
+            })
+        }</div>
+        <div className="flex flex-row place-items-center"><p>Selected: <br />{selected.row && `${selected.row}${selected.start}-${selected.row}${selected.end}`}</p></div>
+    </div>
 }

d5105820092cb24a4e47afccc5dd71c58ff43d4d
t-s-w <tobby.satyarama@gmail.com>
2023-06-28 21:22:13 +0800

    fix bug in event card

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
-export default function EventCard({event}) {
+export default function EventCard({ event }) {
     return (
-    <>
-    {/* <p>{event?.name}</p> */}
-    <Link to="/events/:eventId">{event?.name}</Link>
-    </>
+        <>
+            {/* <p>{event?.name}</p> */}
+            <Link to={"/events/" + event.id}>{event?.name}</Link>
+        </>
     )
 }

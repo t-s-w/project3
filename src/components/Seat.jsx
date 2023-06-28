f806e2afce2b98852d6b61791b500f96a42a19dc
t-s-w <tobby.satyarama@gmail.com>
2023-06-28 19:19:19 +0800

    fix Seat css

--- src/components/Seat.jsx
+++ src/components/Seat.jsx
@@ -1,5 +1,5 @@
 
 export default function Seat(props) {
     const { row, num, onClick, selected } = props;
-    return <div className={`h-6 w-6 m-[2px] text-[0.7rem] rounded-full bg-orange-${selected ? 300 : 100} hover:bg-orange-300 flex place-items-center place-content-center select-none`} onClick={onClick}><p>{row + num}</p></div>
+    return <div className={`h-6 w-6 m-[2px] text-[0.7rem] rounded-full bg-orange-100 hover:bg-orange-300 flex place-items-center place-content-center select-none` + (selected ? ' bg-orange-300' : "")} onClick={onClick} > <p>{row + num}</p></div >
 }

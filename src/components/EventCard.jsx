c6afe33a05711b36ecb6ce25e8bf2c3ecd1fe786
ilyas182 <ilyas2104@gmail.com>
2023-06-28 21:52:15 +0800

    display id using UseParams in event details page

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
+import "./eventcard.css"
 
 export default function EventCard({event}) {
     return (
     <>
-    <img 
-    src={event?.images[0].url}
-    width={200}
-    
-    />
-    <Link to="/events/:eventId">{event?.name}</Link>
-
+    <div className="event-container">
+        <div className="event-card-item">
+        <img src={event?.images[0].url} width={300} />
+        <Link to={`/events/${event?.id}`}>{event?.name}</Link>
+        </div>
+    </div>
     </>
     )
 }
--- src/pages/EventDetailsPage/EventDetailsPage.jsx
+++ src/pages/EventDetailsPage/EventDetailsPage.jsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
 export default function EventDetailsPage(){
+
+    const {id} = useParams();
+    
     return (
     <>
-    Event Details
+    <p>{id}</p>
     <button className="rounded-full m-5 bg-blue-500" >Purchase</button>
     </>
     )

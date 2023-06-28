5417d5b1f0fad578d248fae1ebb5ee8d1576475b
ilyas182 <ilyas2104@gmail.com>
2023-06-28 14:53:43 +0800

    create events page & route

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import SeatSelect from '../SeatSelect/SeatSelect'
+import AllEventsPage from '../AllEventsPage/AllEventsPage.jsx'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<h1>The quick brown fox jumps over the lazy dog</h1>} />
       <Route path="/seatselect" element={<SeatSelect />} />
+      <Route path="/events" element={<AllEventsPage/>} />
     </Routes>
   )
 }

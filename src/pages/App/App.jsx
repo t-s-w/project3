4794709827663c1dcd28796e47d0c25df81e92d9
Theophania <pha.randomacc@gmail.com>
2023-06-30 11:02:56 +0800

    NavBar

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,17 +4,24 @@ import SeatSelect from '../SeatSelect/SeatSelect'
 import AllEventsPage from '../AllEventsPage/AllEventsPage.jsx'
 import EventDetailsPage from '../EventDetailsPage/EventDetailsPage';
 import './App.css'
+import NavBar from "../../components/NavBar";
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<h1>The quick brown fox jumps over the lazy dog</h1>} />
-      <Route path="/seatselect" element={<SeatSelect />} />
-      <Route path="/events" element={<AllEventsPage/>} />
-      <Route path="/events/:id" element={<EventDetailsPage />} /> 
-    </Routes>
-  )
+    <>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={<h1>The quick brown fox jumps over the lazy dog</h1>}
+        />
+        <Route path="/seatselect" element={<SeatSelect />} />
+        <Route path="/events" element={<AllEventsPage />} />
+        <Route path="/events/:id" element={<EventDetailsPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App

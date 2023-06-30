0e1a2c164493aea741bab87812d35963b7d80b63
t-s-w <tobby.satyarama@gmail.com>
2023-06-30 17:31:25 +0800

    fix seating css

--- src/components/Seat.jsx
+++ src/components/Seat.jsx
@@ -1,9 +1,9 @@
 
 const gradeColours = [
     ['bg-slate-400', 'bg-slate-200 hover:bg-slate-400'],
-    ['bg-red-400', 'bg-red-200 hover:bg-slate-400'],
-    ['bg-amber-400', 'bg-amber-200 hover:bg-amber400'],
-    ['bg-emerald-400', 'bg-emerald-200 hover:bg-emerald 400'],
+    ['bg-red-400', 'bg-red-200 hover:bg-red-400'],
+    ['bg-amber-400', 'bg-amber-200 hover:bg-amber-400'],
+    ['bg-emerald-400', 'bg-emerald-200 hover:bg-emerald-400'],
     ['bg-sky-400', 'bg-sky-200 hover:bg-sky-400'],
     ['bg-indigo-400', 'bg-indigo-200 hover:bg-indigo-400']
 ]
@@ -13,5 +13,6 @@ export default function Seat(props) {
     const baseStyle = 'h-8 w-8 m-[2px] text-[0.7rem] rounded-md border-slate-400 border-2 p-2 flex place-items-center place-content-center select-none '
     const colourStyle = gradeColours[grade][1 - selected]
     const classes = baseStyle + colourStyle
+    console.log(classes)
     return <div className={classes} onClick={onClick} > <p>{row + num}</p></div >
 }

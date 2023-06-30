fcd634d9a239bca27c6a3a643cb064ad7ac00447
t-s-w <tobby.satyarama@gmail.com>
2023-06-30 11:35:37 +0800

    Add jsonwebtoken to users controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,21 @@
 import User from '../models/User.js'
 import jwt from 'jsonwebtoken'
 
+function createJWT(user) {
+    return jwt.sign(
+        // data payload
+        { user },
+        process.env.SECRET,
+        { expiresIn: '24h' }
+    );
+}
+
 const userCtrl = ({
     create: async function (req, res) {
         try {
             const user = await User.create(req.body);
+            const token = createJWT(user);
+            res.json(token);
         } catch (err) {
             res.status(400).json(err);
         }

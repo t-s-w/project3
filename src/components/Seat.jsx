cf44209e7ff98c7587e49c678afdc38b380c6673
t-s-w <tobby.satyarama@gmail.com>
2023-07-01 11:21:48 +0800

    add dark mode colors to seat selector

--- src/components/Seat.jsx
+++ src/components/Seat.jsx
@@ -1,11 +1,11 @@
 
 const gradeColours = [
-    ['bg-slate-400', 'bg-slate-200 hover:bg-slate-400'],
-    ['bg-red-400', 'bg-red-200 hover:bg-red-400'],
-    ['bg-amber-400', 'bg-amber-200 hover:bg-amber-400'],
-    ['bg-emerald-400', 'bg-emerald-200 hover:bg-emerald-400'],
-    ['bg-sky-400', 'bg-sky-200 hover:bg-sky-400'],
-    ['bg-indigo-400', 'bg-indigo-200 hover:bg-indigo-400']
+    ['bg-slate-500 dark:bg-slate-600', 'bg-slate-200 hover:bg-slate-400'],
+    ['bg-red-500 dark:bg-red-600', 'bg-red-200 hover:bg-red-400 dark:hover:bg-red-700 dark:bg-red-950'],
+    ['bg-amber-500 dark:bg-amber-600', 'bg-amber-200 hover:bg-amber-400 dark:hover:bg-amber-700 dark:bg-amber-950'],
+    ['bg-emerald-500 dark:bg-emerald-600', 'bg-emerald-200 hover:bg-emerald-400 dark:hover:bg-emerald-700 dark:bg-emerald-950'],
+    ['bg-sky-500 dark:bg-sky-600', 'bg-sky-200 hover:bg-sky-400 dark:hover:bg-sky-700 dark:bg-sky-900'],
+    ['bg-indigo-500 dark:bg-indigo-600', 'bg-indigo-200 hover:bg-indigo-400 dark:hover:bg-indigo-700 dark:bg-indigo-900']
 ]
 
 export default function Seat(props) {

5bc0016d4951eb67fcbf9a3d4d6841cdc89b807b
ilyas182 <ilyas2104@gmail.com>
2023-07-02 21:01:22 +0800

    Link Events by Category page to Event Details

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom"
 
 export default function Category (props){
-    console.log(props.categoryArray)
+    
     return (
     <div>
     <h2><Link 
     to={`/events/categories/categories/${props.categoryName}`}
-    state={{categoryArray: props.categoryArray}}
+    state={{categoryArray: props.categoryArray,
+            categoryName: props.categoryName
+            }}
     >{props.categoryName}</Link></h2>
     </div>
     )
--- src/pages/CategoriesPage/EventsByCategory.jsx
+++ src/pages/CategoriesPage/EventsByCategory.jsx
@@ -1,12 +1,21 @@
 import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
 
 export default function EventsByCategory(){
     const location = useLocation();
-    const {categoryArray} = location.state;
-    console.log(categoryArray);
+    const {categoryArray, categoryName} = location.state;
+    console.log(categoryArray)
     return (
     <>
-    {JSON.stringify(categoryArray)}
+    <h1>{categoryName}</h1>
+    {/* {JSON.stringify(categoryArray)} */}
+    {categoryArray.map(category => ( 
+        <div className="w-1/3 p-3">
+            <img className="w-full h-40 object-cover" src={category.images[0].url} />
+            <Link to={`/events/${category?._id}`}>{category?.name}</Link>
+            
+        </div>
+    ))}
     </>
     )
 }

2addf6f1041e133a62bc5d1bd8c5509c2f55671d
t-s-w <tobby.satyarama@gmail.com>
2023-07-02 13:56:15 +0800

    Add dropdown functionality to logging in

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,4 +1,20 @@
-export default function LoginButton(props) {
-    const { user } = props;
-    return "⌾ Sign-in/Register"
+import { UserContext } from "../pages/App/App";
+import { useContext } from "react";
+import LoginForm from './LoginForm'
+import './dropdown.css'
+
+function NotLoggedIn() {
+    return <div className="relative w-60" >
+        <label htmlFor="dropdown"><span className="cursor-pointer">⌾ Log in</span></label>
+        <input type="checkbox" id="dropdown" className="hidden" />
+        <ul className="border-box px-5 rounded-b-lg slide clear-both width-full h-0 overflow-hidden transition-[height] absolute bg-blue-800">
+            <LoginForm />
+        </ul>
+    </div>
+}
+
+export default function LoginButton() {
+    const { user } = useContext(UserContext);
+
+    return user ? `Signed in as ${user.email}` : <NotLoggedIn />
 }
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,6 +2,7 @@ import { useState, useContext } from "react";
 import * as usersSvc from '../utilities/users-service'
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../pages/App/App";
+import { Link } from "react-router-dom";
 
 export default function LoginForm() {
     const { setUser } = useContext(UserContext);
@@ -16,22 +17,22 @@ export default function LoginForm() {
             setFormState('loading')
             setError(null)
             const user = await usersSvc.login(credentials);
-            setSuccessMsg('Sign up successful! Returning you to the home page soon...')
-            setUser(user);
-            setTimeout(() => navigate('/'), 2000);
+            setSuccessMsg('Successfully logged in!')
+            setTimeout(() => { setUser(user); navigate('/') }, 2000);
         } catch (err) {
             setFormState('idle')
             setError(err.message)
         }
     }
-    return <div className="loginContainer">
+    return <div className="loginContainer w-full mt-5">
         <form onSubmit={handleSubmit}>
             <fieldset disabled={formState === "loading"} className="flex flex-col place-items-center place-content-center [&>*]:m-1">
-                <input className="p-2 pl-4 border-solid border-2 rounded-full" type="email" name="email" placeholder="E-mail" required />
-                <input className="p-2 pl-4 border-solid border-2 rounded-full" type="password" name="password" placeholder="Password" required />
+                <input className="p-2 pl-4 border-solid border-2 rounded-full w-full text-slate-500 dark:text-darkDefault" type="email" name="email" placeholder="E-mail" required />
+                <input className="p-2 pl-4 border-solid border-2 rounded-full w-full text-slate-500 dark:text-darkDefault" type="password" name="password" placeholder="Password" required />
                 <button className="w-fit bg-slate-300 font-bold disabled:bg-slate-200 disabled:text-slate-700">Log in</button>
+                <p>No account yet? <Link to="/signup">Sign up</Link></p>
                 {error && <p className="text-red-700">{error}</p>}
-                {successMsg && <p className="text-green-700">{successMsg}</p>}
+                {successMsg && <p className="text-emerald-300">{successMsg}</p>}
             </fieldset>
         </form>
     </div>

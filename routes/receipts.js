bfaf81618746c07e3ba1088e56b729f32d1bfbd8
t-s-w <tobby.satyarama@gmail.com>
2023-07-03 16:17:41 +0800

    add delete route for receipts

--- routes/receipts.js
+++ routes/receipts.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post('/', receiptsCtrl.purchase)
 router.get('/byUser',receiptsCtrl.getReceiptsByUser)
 router.get('/:id', receiptsCtrl.getReceiptWithEvent)
+router.delete('/:id',receiptsCtrl.cancel)
 
 
 export default router

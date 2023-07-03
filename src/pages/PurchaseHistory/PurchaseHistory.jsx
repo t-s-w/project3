8bb1251025dba48074ef1a88fada4dfe1d7eff41
t-s-w <tobby.satyarama@gmail.com>
2023-07-03 21:48:58 +0800

    add condition to useeffect to refresh page

--- src/pages/PurchaseHistory/PurchaseHistory.jsx
+++ src/pages/PurchaseHistory/PurchaseHistory.jsx
@@ -7,6 +7,7 @@ export default function PurchaseHistory() {
     const [cancelBooking, setCancelBooking] = useState(null)
     const [receipts, setReceipts] = useState([])
     const [pageStatus, setPageStatus] = useState(null)
+    const [refreshPage, setRefreshPage] = useState(0)
     async function getReceipts() {
         try {
             setPageStatus('Loading...')
@@ -19,14 +20,14 @@ export default function PurchaseHistory() {
     }
     useEffect(() => {
         getReceipts()
-    }, [])
+    }, [refreshPage])
     return pageStatus ? <h1>{pageStatus}</h1> :
         <>
             <div className="flex flex-col [&>div]:my-2 place-items-center">
                 <h1 className="mt-8">Purchase History</h1>
                 {receipts.map(receipt => <div key={receipt._id} className="w-full flex flex-row justify-center place-items-center [&>*]:mx-4"><PurchaseCard receipt={receipt} /><button className="h-fit" onClick={() => setCancelBooking(receipt)}>Cancel</button></div>)}
             </div>
-            {cancelBooking ? <ConfirmCancel receipt={cancelBooking} setCancelBooking={setCancelBooking} /> : null}
+            {cancelBooking ? <ConfirmCancel receipt={cancelBooking} setCancelBooking={setCancelBooking} setRefreshPage={setRefreshPage} /> : null}
         </>
 
 }

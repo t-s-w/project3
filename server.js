685037cd68b04fae3404d86eb2e1b6c2c5badef7
t-s-w <tobby.satyarama@gmail.com>
2023-07-03 10:33:44 +0800

    Add token checking middleware

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ app.use(express.json());
 // app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, "dist")));
 
+// Middleware to verify token and assign user object of payload to req.user.
+// Be sure to mount before routes
+app.use(require('./config/checkToken'));
+
 // Put API routes here, before the "catch all" route
 
 

561d5e941c6b72c7f99e5a9ca74dc8e8b4812d01
t-s-w <tobby.satyarama@gmail.com>
2023-07-04 23:48:17 +0800

    improve receipts model to take more parameters

--- controllers/receipts.js
+++ controllers/receipts.js
@@ -4,35 +4,51 @@ import jwt from 'jsonwebtoken';
 
 export async function purchase(req, res) {
   try {
-    const pastReceipts = await Receipt.find({ eventId: req.body.eventId, row: req.body.row })
-    for (let receipt of pastReceipts) {
-      if (req.body.startSeat <= receipt.endSeat && (req.body.endSeat >= receipt.startSeat)) {
-        res.status(400).json({ message: "Some of the selected seats are unavailable" })
-        return
+    if (!req.user) { res.status(401).json({ message: 'Not logged in' }); return }
+    const receipt = {
+      deliveryName: req.body.deliveryName,
+      deliveryAddress: req.body.deliveryAddress,
+      deliveryContactNo: req.body.deliveryContactNo
+    }
+    jwt.verify(req.body.purchaseInfo, process.env.SECRET, function (err, decoded) {
+      if (!err) {
+        const verified = decoded.verifiedReceipt
+        receipt.row = verified.row
+        receipt.startSeat = verified.startSeat
+        receipt.endSeat = verified.endSeat
+        receipt.amountPaid = verified.amountPaid
+        receipt.eventId = verified.eventId
+        receipt.amountPaid = verified.amountPaid
+        receipt.customerId = req.user._id
+      }
+    })
+    const pastReceipts = await Receipt.find({ eventId: receipt.eventId, row: receipt.row })
+    for (let rcpt of pastReceipts) {
+      if (receipt.startSeat <= rcpt.endSeat && (receipt.endSeat >= rcpt.startSeat)) {
+        throw new Error('Some of the selected seats are unavailable')
       }
     }
-    const acceptedReceipt = await Receipt.create(req.body);
+    const acceptedReceipt = await Receipt.create(receipt);
     const populatedReceipt = await Receipt.findById(acceptedReceipt._id).populate('eventId');
-    console.log(populatedReceipt);
     res.json(populatedReceipt);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
 }
 
-export async function verifyPurchase(req,res) {
+export async function verifyPurchase(req, res) {
   try {
-    if(!req.user) {res.status(401).json({message:'Not logged in'}); return}
+    if (!req.user) { res.status(401).json({ message: 'Not logged in' }); return }
 
-    if(!req.body.row || !req.body.startSeat || !req.body.endSeat || !req.body.eventId) {res.status(400).json({message:"Invalid request"}); return}
+    if (!req.body.row || !req.body.startSeat || !req.body.endSeat || !req.body.eventId) { res.status(400).json({ message: "Invalid request" }); return }
 
     const checkEvent = await Event.findById(req.body.eventId)
-    if (!checkEvent) {res.status(400).json({message: "Event id not found"}); return}
+    if (!checkEvent) { res.status(400).json({ message: "Event id not found" }); return }
 
-    const pastReceipts = await Receipt.find({eventId: req.body.eventId, row: req.body.row})
+    const pastReceipts = await Receipt.find({ eventId: req.body.eventId, row: req.body.row })
     for (let receipt of pastReceipts) {
-      if(req.body.startSeat <= receipt.endSeat && (req.body.endSeat >= receipt.startSeat)) {
-        res.status(400).json({message: "Some of the selected seats are unavailable"})
+      if (req.body.startSeat <= receipt.endSeat && (req.body.endSeat >= receipt.startSeat)) {
+        res.status(400).json({ message: "Some of the selected seats are unavailable" })
         return
       }
     }
@@ -43,16 +59,16 @@ export async function verifyPurchase(req,res) {
       endSeat: req.body.endSeat,
       customerId: req.user._id,
       physical: true,
-      row:req.body.row,
+      row: req.body.row,
       amountPaid: req.body.amountPaid
     }
 
-    const purchaseToken = jwt.sign({verifiedReceipt},process.env.SECRET
-      )
+    const purchaseToken = jwt.sign({ verifiedReceipt }, process.env.SECRET
+    )
 
     res.json(purchaseToken)
-  } catch(err) {
-    res.status(400).json({message: err.message})
+  } catch (err) {
+    res.status(400).json({ message: err.message })
   }
 }
 
@@ -62,15 +78,15 @@ export async function cancel(req, res) {
   console.log('cancel attempt')
   try {
     const receipt = await Receipt.findById(id);
-    if(!receipt) {
-      res.status(404).json({message: "Requested receipt id not found"})
+    if (!receipt) {
+      res.status(404).json({ message: "Requested receipt id not found" })
       return
     }
     if (!receipt.customerId.equals(userId)) {
-      res.status(403).json({message: "You are not authorised to make this request"})
+      res.status(403).json({ message: "You are not authorised to make this request" })
       return
     }
-    const response = await Receipt.deleteOne({_id: id})
+    const response = await Receipt.deleteOne({ _id: id })
     res.json(response);
   } catch (err) {
     res.status(400).json({ message: err.message })
@@ -90,16 +106,16 @@ export async function getReceiptWithEvent(req, res) {
   }
 }
 
-export async function getReceiptsByUser(req,res) {
+export async function getReceiptsByUser(req, res) {
   const userId = req.user._id
   if (!userId) {
-    res.status(401).json({message: "Not logged in"})
+    res.status(401).json({ message: "Not logged in" })
   }
   try {
-    const receipts = await Receipt.find({customerId: userId}).populate('eventId');
-    receipts.sort((x,y) => y.createdAt > x.createdAt ? 1 : -1)
+    const receipts = await Receipt.find({ customerId: userId }).populate('eventId');
+    receipts.sort((x, y) => y.createdAt > x.createdAt ? 1 : -1)
     res.json(receipts)
-  } catch(err) {
-    res.status(400).json({message:err.message})
+  } catch (err) {
+    res.status(400).json({ message: err.message })
   }
 }

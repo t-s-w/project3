ca07a1050b625946b997171e2bb89216a356c12b
t-s-w <tobby.satyarama@gmail.com>
2023-07-04 15:13:29 +0800

    confirm purchase page returns error page if purchase token invalid

--- src/pages/ConfirmPurchasePage/ConfirmPurchasePage.jsx
+++ src/pages/ConfirmPurchasePage/ConfirmPurchasePage.jsx
@@ -1,10 +1,17 @@
 import sendRequest from "../../utilities/send-request"
 
 export default function ConfirmPurchasePage() {
-    const purchaseToken = localStorage.getItem('purchaseToken');
-    if(!purchaseToken) {
-        return <>
+    try {
+        const purchaseToken = localStorage.getItem('purchaseToken');
+        const verifiedPurchase = atob(purchaseToken.split('.')[1])
+    }
+    catch {
+            return <>
         <h2>Something went wrong!</h2>
         </>
-    }
+}
+        return <>
+        <h1>Confirm Purchase</h1>
+        </>
+    
 }

3500a402eb004a155fac56db26cb7bba6d980a5f
t-s-w <tobby.satyarama@gmail.com>
2023-07-04 14:41:30 +0800

    add confirm purchase page route

--- src/pages/ConfirmPurchasePage/ConfirmPurchasePage.jsx
+++ src/pages/ConfirmPurchasePage/ConfirmPurchasePage.jsx
@@ -1,5 +1,10 @@
 import sendRequest from "../../utilities/send-request"
 
-export default ConfirmPurchasePage() {
-
+export default function ConfirmPurchasePage() {
+    const purchaseToken = localStorage.getItem('purchaseToken');
+    if(!purchaseToken) {
+        return <>
+        <h2>Something went wrong!</h2>
+        </>
+    }
 }

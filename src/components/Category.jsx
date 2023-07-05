412852a119ba7ced0cc51d538dbe985903adec9a
ilyas182 <ilyas2104@gmail.com>
2023-07-05 16:50:32 +0800

    add loading icon

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom"
 export default function Category (props){
     
     return (
-    <div className="underline decoration-transparent transition duration-300 ease-in-out hover:decoration-inherit">
-    <h2><Link 
-    to={`/events/categories/categories/${props.categoryName}`}
-    state={{categoryArray: props.categoryArray,
+    <div>
+        
+        <div className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+            <h2><Link 
+            to={`/events/categories/categories/${props.categoryName}`}
+            state={{categoryArray: props.categoryArray,
             categoryName: props.categoryName
             }}
-    >{props.categoryName}</Link></h2>
+            >{props.categoryName}</Link></h2>
+        </div>
     </div>
     )
 }

f2d63c0760611e08de8492b7dfc5d104eaec0785
ilyas182 <ilyas2104@gmail.com>
2023-07-05 10:33:31 +0800

    image slider for upcoming events

--- src/components/UpcomingEvents.jsx
+++ src/components/UpcomingEvents.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
 export default function UpcomingEvents ({events}) {
     const sortedEvents = events.sort(function(a,b){
         // Turn your strings into dates, and then subtract them
@@ -10,30 +14,44 @@ export default function UpcomingEvents ({events}) {
       });
       console.log(sortedEvents);
 
-    //   const options = {
-    //     weekday: "long",
-    //     year: "numeric",
-    //     month: "long",
-    //     day: "numeric",
-    //     timeZone: "America/Indianapolis",
-    //     timeZoneName: "short"
-    //   };
-    //     const dateObj = new Date(event.dates.start.dateTime);
-    //     const dateStr = dateObj ? dateObj.toLocaleString("en-GB",options) : '';
-    //     const timeStr = dateObj ? dateObj.toLocaleTimeString("en-US", { timeZone:"America/Indianapolis", timeZoneName: "short" }) : '';
+      const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",       
+      };
+    ;
 
     return (
     <>
-    Upcoming Events
-    <div className="flex flex-nowrap snap-x scroll-smooth slideshow-container">
-    {sortedEvents.slice(0,5).map((event, i) => 
-        <div className="w-1/3 p-3 mySlides fade" >
-            {/* <div class="numbertext">{i+1} / 5</div> */}
-            <img className="w-full h-48 object-cover" src={event?.images[0].url} />
-            <div class="text">{event.name}</div>
-        </div>
-    )}
-    </div>
+    
+    
+    
+    <h2>Upcoming Events</h2>
+
+    <Carousel showThumbs={false} showStatus={false} showIndicators={false}>
+        {sortedEvents.slice(0, 5).map((event, i) => {
+          const dateISO = event?.dates?.start?.dateTime;
+          const dateObj = dateISO ? new Date(dateISO) : null;
+          const dateStr = dateObj ? dateObj.toLocaleString('en-GB', options) : '';
+          const timeStr = dateObj
+            ? dateObj.toLocaleTimeString('en-US', {
+                timeZone: event?._embedded?.venues[0]?.timezone,
+                timeZoneName: 'long',
+              })
+            : '';
+
+          return (
+            <div key={event.id} className="carousel-item">
+              <img className="w-full h-48 object-cover" src={event?.images[0].url} alt={event.name} />
+              <p>{event.name}</p>
+              <p>{dateStr}</p>
+              <p>{timeStr}</p>
+            </div>
+          );
+        })}
+      </Carousel>
+    
     </>
     )
 }

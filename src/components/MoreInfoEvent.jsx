bdc94dc3dbc9889901d3b575d447aa7cb38731e6
Theophania <pha.randomacc@gmail.com>
2023-07-05 15:56:13 +0800

    More info CSS

--- src/components/MoreInfoEvent.jsx
+++ src/components/MoreInfoEvent.jsx
@@ -1,17 +1,33 @@
 import "./moreinfoevent.css"
 export default function MoreInfoEvent(props) {
     
-    return (props.trigger) ?  (
-    <div className='popup'>
-        <div className='popup-inner bg-white dark:bg-stone-700'>
-            <button className='close-btn rounded-lg' onClick={() => props.setTrigger(false)}>X</button>
-            <p>Please note: {props.event?.pleaseNote}</p>
-        
-            <hr/>
-            <br/>
-            <p>Ticket limit: {props.event?.ticketLimit?.info}</p>
-            <p>Tickets are sold in {props?.event?.priceRanges[0]?.currency}</p>
+    return props.trigger ? (
+      <div className="popup">
+        <div className="popup-inner bg-white dark:bg-slate-800">
+          <button
+            className="close-btn rounded-lg"
+            onClick={() => props.setTrigger(false)}
+          >
+            X
+          </button>
+          <div className="m-5 text-left">
+            <h1 className="text-extrabold text-xl">ⓘ More information</h1>
+            <br />
+            <div className="m-5">
+              <p>Ticket limit: {props.event?.ticketLimit?.info}</p>
+              <p>
+                Tickets are sold in{" "}
+                <b>{props?.event?.priceRanges[0]?.currency}.</b>
+              </p>{" "}
+            </div>
+            <hr />
+            <div className="m-5">
+              <p>***Please note: {props.event?.pleaseNote}</p>
+            </div>
+          </div>
         </div>
-    </div>
-    ) : "";
+      </div>
+    ) : (
+      ""
+    );
 }

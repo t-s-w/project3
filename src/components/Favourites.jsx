e14d7048f06395a83c4e790aa660cde80321bae9
Theophania <pha.randomacc@gmail.com>
2023-07-05 21:42:04 +0800

    Favourites popup

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -2,6 +2,7 @@ import starImage from "../../dist/star.png";
 import { UserContext } from "../pages/App/App";
 import { useContext, useState } from "react";
 import sendRequest from "../utilities/send-request";
+import { Link } from "react-router-dom";
 
 export default function Favourites({ event }) {
   const { user, setUser } = useContext(UserContext);
@@ -9,11 +10,9 @@ export default function Favourites({ event }) {
   console.log(event);
 
   const handleClick = async function () {
-    const eventId = event._id;
-
     try {
       const response = await sendRequest("/api/userDetails", "PATCH", {
-        favourites: eventId,
+        favourites: event,
       });
       console.log(response); // Process the response data as needed
       setSuccess(true);
@@ -30,8 +29,30 @@ export default function Favourites({ event }) {
           onClick={handleClick}
           className="bg-transparent flex items-center"
         >
-          <img src={starImage} alt="Star" className="h-20 w-20" />
+          <img src={starImage} alt="Star" className="h-10 w-10" />
         </button>
+        {success && (
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="bg-white dark:bg-slate-800 p-10 rounded-lg w-96 relative">
+              <button
+                className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+                onClick={() => setSuccess(false)}
+              >
+                x
+              </button>
+
+              <h1 className="font-bold text-xl mb-4">Added to favourites!</h1>
+
+              <div className="text-center">
+                <Link to="/foryou">
+                  <button className="rounded-lg bg-blue-500 hover:bg-blue-600 text-white px-4 py-2">
+                    View favourites
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     )
   );
--- src/pages/ForYouPage/ForYouPage.jsx
+++ src/pages/ForYouPage/ForYouPage.jsx
@@ -1,4 +1,15 @@
 export default function ForYouPage() {
+  //   useEffect(() => {
+  //     async function fetchAllEvents() {
+  //         const response = await fetch("/api/events");
+  //         const jsonData = await response.json();
+  //         setEvents(jsonData);
+  //         setLoading(false);
+  //     }
+  //     fetchAllEvents();
+  //     console.log(events);
+  // }, []);
+
   return (
     <>
       <h1>For you</h1>

d1c804be3e68629fd5ef4694b7306d583526f87d
ilyas182 <ilyas2104@gmail.com>
2023-07-05 19:07:14 +0800

    change style for headers

--- src/components/UpcomingEvents.jsx
+++ src/components/UpcomingEvents.jsx
@@ -38,7 +38,7 @@ export default function UpcomingEvents({ events }) {
 
 
 
-      <h2 className="text-left text-2xl my-2">Upcoming Events</h2>
+      <h2 className="text-left text-2xl my-2 text-left text-1xl my-2 mb-4 font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-white underline underline-offset-3 decoration-8 decoration-blue-400 dark:decoration-blue-600">Upcoming Events</h2>
       <div className="">
         <Carousel showThumbs={false} showStatus={true} showIndicators={false}>
           {sortedEvents.slice(0, 5).map((event, i) => {

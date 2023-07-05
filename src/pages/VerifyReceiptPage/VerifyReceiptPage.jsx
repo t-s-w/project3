6e5b89b4b70eb2a3c08fdc4d6c7ac749724c2e84
t-s-w <tobby.satyarama@gmail.com>
2023-07-05 22:18:45 +0800

    add details to verify receipt page

--- src/pages/VerifyReceiptPage/VerifyReceiptPage.jsx
+++ src/pages/VerifyReceiptPage/VerifyReceiptPage.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react"
 export default function VerifyReceiptPage() {
     const { id } = useParams()
     const [receipt, setReceipt] = useState();
-    const [pageState, setPageState] = useState('idle');
+    const [pageState, setPageState] = useState('Loading...');
     async function getReceipt() {
         try {
             setPageState('Loading...')
@@ -13,12 +13,20 @@ export default function VerifyReceiptPage() {
             setReceipt(receipt)
             setPageState('idle')
         } catch (err) {
-            setPageState(err.message)
+            setPageState("Error: invalid receipt")
         }
 
     }
     useEffect(() => {
         getReceipt()
     }, [])
-    return <h1>{pageState}</h1>
+
+    return pageState === "idle" ? <div className="text-left mb-auto mt-10 flex flex-col justify-start">
+        <h1 className="mb-4">Verify Receipt</h1>
+        <p><span className="font-bold">Name:</span> {receipt.deliveryName}</p>
+        <p><span className="font-bold">Seat(s):</span> {receipt.row + (receipt.startSeat < 10 ? '0' + receipt.startSeat : receipt.startSeat)}{(receipt.endSeat !== receipt.startSeat) && '-' + (receipt.row + (receipt.endSeat < 10 ? '0' + receipt.endSeat : receipt.endSeat))}</p>
+        <p><span className="font-bold">Purchased on: </span>{(new Date(receipt.createdAt)).toLocaleTimeString('en-sg', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' })}</p>
+        <p><span className="font-bold">Amount paid: </span> ${receipt.amountPaid}</p>
+    </div>
+        : <h1>{pageState}</h1>
 }

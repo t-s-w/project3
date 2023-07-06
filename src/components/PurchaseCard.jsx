d1a0c55cc33883bbb72f3b1278b830938149fe72
t-s-w <tobby.satyarama@gmail.com>
2023-07-06 11:41:15 +0800

    handle nonspecific dates in purchase cards

--- src/components/PurchaseCard.jsx
+++ src/components/PurchaseCard.jsx
@@ -3,8 +3,8 @@ import QRCode from "react-qr-code"
 export default function PurchaseCard({ receipt }) {
     const event = receipt.eventId
     const eventDateTime = (new Date(event.dates.start.dateTime))
-    const eventDate = eventDateTime.toLocaleDateString('en-sg', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
-    const eventTime = eventDateTime.toLocaleTimeString('en-sg', { hour: 'numeric', minute: 'numeric', timeZone: event.dates.timezone })
+    const eventDate = event.dates.start.dateTime ? eventDateTime.toLocaleDateString('en-sg', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }) : "No specific date"
+    const eventTime = event.dates.start.dateTime ? eventDateTime.toLocaleTimeString('en-sg', { hour: 'numeric', minute: 'numeric', timeZone: event.dates.timezone }) : null
     const gridLayout = receipt._id ? 'grid-cols-[2fr_8fr_2fr]' : 'grid-cols-[2fr_8fr]'
     return <div className={`max-w-4xl w-full grid ${gridLayout} text-left min-h-md border-double rounded-lg border-slate-500 border-4`}>
         <div className="bg-cover bg-center" style={{ backgroundImage: `url('${event.images[0].url}')` }}></div>

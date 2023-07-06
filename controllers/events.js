ef47d38a7f3baa55d5b6d858b5de922271a8d81f
Theophania <pha.randomacc@gmail.com>
2023-07-06 15:00:24 +0800

    Recommendations

--- controllers/events.js
+++ controllers/events.js
@@ -1,81 +1,118 @@
 import Event from '../models/Event.js';
 import Receipt from '../models/Receipt.js'
 import vars from '../config/vars.js';
+import UserDetail from "../models/UserDetail.js";
 // import { events } from 'mongoose';
 
-
 async function getAll(req, res) {
-    const events = await Event.find();
-    res.json(events);
+  const events = await Event.find();
+  res.json(events);
 }
 async function refreshData(req, res) {
-    const eventData = await fetch(vars.TICKETMASTER_API).then(res => res.json())
-    eventData._embedded.events.forEach(async x => {
-        const y = await Event.replaceOne({ id: x.id }, x, { upsert: true })
-    })
+  const eventData = await fetch(vars.TICKETMASTER_API).then((res) =>
+    res.json()
+  );
+  eventData._embedded.events.forEach(async (x) => {
+    const y = await Event.replaceOne({ id: x.id }, x, { upsert: true });
+  });
 }
 async function findById(req, res) {
-    const { id } = req.params;
-    try {
-        const event = await Event.findById(id);
-        if (!event) {
-            res.status(404).json({ "msg": "Requested event id not found" })
-        }
-        res.json(event)
-    } catch {
-        res.status(404).json({ "msg": "Id not found!" })
+  const { id } = req.params;
+  try {
+    const event = await Event.findById(id);
+    if (!event) {
+      res.status(404).json({ msg: "Requested event id not found" });
     }
+    res.json(event);
+  } catch {
+    res.status(404).json({ msg: "Id not found!" });
+  }
 }
 
 async function getTakenSeats(req, res) {
-    try {
-
-        const { id } = req.params;
-        const eventReceipts = await Receipt.find({ eventId: id });
-        const returnArr = []
-        for (let receipt of eventReceipts) {
-            for (let i = receipt.startSeat; i <= receipt.endSeat; i++) {
-                const seatNum = i < 10 ? '0' + i.toString() : i.toString()
-                returnArr.push(receipt.row + seatNum)
-            }
-        }
-        res.json(returnArr)
-    } catch (err) {
-        res.status(400).json({ message: err.message })
+  try {
+    const { id } = req.params;
+    const eventReceipts = await Receipt.find({ eventId: id });
+    const returnArr = [];
+    for (let receipt of eventReceipts) {
+      for (let i = receipt.startSeat; i <= receipt.endSeat; i++) {
+        const seatNum = i < 10 ? "0" + i.toString() : i.toString();
+        returnArr.push(receipt.row + seatNum);
+      }
     }
+    res.json(returnArr);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 }
 
 async function getAllCategories(req, res) {
-    try {
-    const categories  = await Event.distinct('classifications.segment.name');
+  try {
+    const categories = await Event.distinct("classifications.segment.name");
     let eventsByCategory = {};
-    
+
     for (const category of categories) {
-      const events = await Event.find({ 'classifications.segment.name': category });
+      const events = await Event.find({
+        "classifications.segment.name": category,
+      });
       eventsByCategory[category] = events;
     }
 
     res.json(eventsByCategory);
-    } catch (error) {
-    res.status(500).json({ message: 'Error retrieving events' });
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving events" });
   }
 }
 
-async function findBySearch(req,res) { 
-    const { searchResults } = req.params;
+async function findBySearch(req, res) {
+  const { searchResults } = req.params;
 
-    try {
-        // const events = await Event.find({"name": {"$regex": searchResults, "$options": "i"}})
-        const events = await Event.find( {$or: [{"name": {"$regex": searchResults, "$options": "i"}},{ 'classifications.segment.name': {"$regex": searchResults, "$options": "i"} }]} )
-        if (events.length === 0) {
-            res.status(404).json({ "msg": "No search results found" })
-        }
-        else {
-        res.json(events)
-        }
-    } catch (error)
-    {
-        res.status(500).json({ message: 'Error retrieving events' });
+  try {
+    // const events = await Event.find({"name": {"$regex": searchResults, "$options": "i"}})
+    const events = await Event.find({
+      $or: [
+        { name: { $regex: searchResults, $options: "i" } },
+        {
+          "classifications.segment.name": {
+            $regex: searchResults,
+            $options: "i",
+          },
+        },
+      ],
+    });
+    if (events.length === 0) {
+      res.status(404).json({ msg: "No search results found" });
+    } else {
+      res.json(events);
     }
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving events" });
+  }
 }
-export { getAll, refreshData, findById, getTakenSeats, getAllCategories, findBySearch }
+
+async function getRecommendations(req, res) {
+  console.log("recommendations");
+  const { topSegment } = req.query;
+  try {
+    const recommendations = await Event.find({
+      "classifications.segment.name": {
+        $regex: new RegExp(topSegment, "i"),
+      },
+    });
+    console.log(recommendations);
+    res.json(recommendations);
+  } catch (error) {
+    console.error("Error fetching recommendations:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export {
+  getAll,
+  refreshData,
+  findById,
+  getTakenSeats,
+  getAllCategories,
+  findBySearch,
+  getRecommendations,
+};
--- src/components/RecommendedCard.jsx
+++ src/components/RecommendedCard.jsx
@@ -1,12 +1,62 @@
-
+import { useEffect, useState } from "react";
 
 export default function Recommended(props) {
   const { favourites, details } = props;
+  const [recommended, setRecommended] = useState([]);
+
+  function countSegments(favourites) {
+    const segmentCounts = {};
+    for (const favourite of favourites) {
+      const segment = favourite.classifications?.[0]?.segment?.name;
+      if (segment) {
+        if (segmentCounts[segment]) {
+          segmentCounts[segment] += 1;
+        } else {
+          segmentCounts[segment] = 1;
+        }
+      }
+    }
+    console.log(segmentCounts);
+    let maxSegment = null;
+    let maxCount = 0;
+
+    for (const segment in segmentCounts) {
+      if (segmentCounts[segment] > maxCount) {
+        maxSegment = segment;
+        maxCount = segmentCounts[segment];
+      }
+    }
+    return maxSegment;
+  }
+
+  const topSegment = countSegments(favourites);
+  console.log("Top segment:", topSegment);
+
+  async function fetchRecommended() {
+    try {
+      const response = await fetch("/api/events/foryou/recommendations");
+      const data = await response.json();
+      //first 10 recos only
+      const recommendations = data.slice(0, 10);
+      setRecommended(recommendations);
+      console.log("10 recos", recommendations);
+    } catch (error) {
+      console.error("Error fetching recommended:", error);
+    }
+  }
+  useEffect(() => {
+    fetchRecommended();
+  }, []);
 
   return (
     <>
       <div className="flex flex-col items-left justify-center h-screen">
-        <div>Recommended based on your favourites</div>
+        <div className="text-2xl font-extrabold">
+          Recommended based on your favourites
+        </div>
+        {recommended.map((reco) => (
+          <div key={reco._id}>{reco.name}</div>
+        ))}
       </div>
     </>
   );

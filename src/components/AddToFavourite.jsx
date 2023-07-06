8fdeed1c353c63b27cae878002be1b2ec764fb4d
Theophania <pha.randomacc@gmail.com>
2023-07-06 19:49:33 +0800

    Show favourited

--- src/components/AddToFavourite.jsx
+++ src/components/AddToFavourite.jsx
@@ -1,14 +1,21 @@
 import { UserContext } from "../pages/App/App";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import sendRequest from "../utilities/send-request";
 import { Link } from "react-router-dom";
 
-export default function AddToFavourite({ event }) {
+export default function AddToFavourite({ event, favourites }) {
   const { user, setUser } = useContext(UserContext);
   const [success, setSuccess] = useState(false);
+  const [favourited, setFavourited] = useState(false);
+
   console.log(event);
+  console.log("fav", favourites);
 
   const handleClick = async function () {
+    if (favourites.includes(event._id)) {
+      setFavourited(true);
+      return;
+    }
     try {
       const response = await sendRequest("/api/userDetails", "PATCH", {
         favourites: event._id,
@@ -41,6 +48,30 @@ export default function AddToFavourite({ event }) {
               </button>
 
               <h1 className="font-bold text-xl mb-4">Added to favourites!</h1>
+
+              <div className="text-center">
+                <Link to="/foryou">
+                  <button className="rounded-lg bg-blue-500 hover:bg-blue-600 text-white px-4 py-2">
+                    View favourites
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        )}
+        {favourited && (
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="bg-white dark:bg-slate-800 p-10 rounded-lg w-96 relative">
+              <button
+                className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+                onClick={() => setFavourited(false)}
+              >
+                x
+              </button>
+
+              <h1 className="font-bold text-xl m-5">
+                -ˋˏ This event is already favourited! ˊˎ-
+              </h1>
 
               <div className="text-center">
                 <Link to="/foryou">
--- src/pages/EventDetailsPage/EventDetailsPage.jsx
+++ src/pages/EventDetailsPage/EventDetailsPage.jsx
@@ -2,63 +2,75 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import MoreInfoEvent from "../../components/MoreInfoEvent";
 import Favourites from "../../components/AddToFavourite";
+import sendRequest from "../../utilities/send-request";
 
 export default function EventDetailsPage() {
-    const { id } = useParams();
-    const [event, setEvent] = useState({});
-    const navigate = useNavigate();
+  const { id } = useParams();
+  const [event, setEvent] = useState({});
+  const navigate = useNavigate();
+  const [favourites, setFavourites] = useState({});
 
+  const [popoutVisible, setPopoutVisible] = useState(false);
 
-    const [popoutVisible, setPopoutVisible] = useState(false);
+  async function getFavourites() {
+    const response = await sendRequest(`/api/userDetails/getOneUser`);
+    setFavourites(response.favourites);
+    console.log(response.favourites);
+  }
+  async function fetchOneEvent() {
+    const response = await fetch(`/api/events/${id}`);
+    const jsonData = await response.json();
+    setEvent(jsonData);
+    console.log("event", jsonData);
+  }
 
-    useEffect(() => {
-      async function fetchOneEvent() {
-        const response = await fetch(`/api/events/${id}`);
-        const jsonData = await response.json();
-        setEvent(jsonData);
-        console.log("event", jsonData);
-      }
-      fetchOneEvent();
-      console.log("event", event);
-    }, []);
+  useEffect(() => {
+    fetchOneEvent();
+    getFavourites();
+    console.log("event", event);
+  }, []);
 
-    const dateISO = event?.dates?.start?.dateTime;
-    const dateObj = dateISO ? new Date(dateISO) : null;
-    const dateStr = dateObj ? dateObj.toUTCString() : "";
-    // console.log(dateObj);
+  const dateISO = event?.dates?.start?.dateTime;
+  const dateObj = dateISO ? new Date(dateISO) : null;
+  const dateStr = dateObj ? dateObj.toUTCString() : "";
+  // console.log(dateObj);
 
-    const handleClick = () => {
-      navigate(`/events/${id}/order`);
-    };
+  console.log("fav", favourites);
+  const handleClick = () => {
+    navigate(`/events/${id}/order`);
+  };
 
-    return (
-      <>
-        {event.images && event.images.length > 0 ? (
-          <img className="mt-2" src={event?.images[0]?.url} />
-        ) : (
-          <img src="" alt="No image available" />
-        )}
-        <Favourites event={event} />
-        <p>{event?.name}</p>
-        <p>{dateStr}</p>
-        <p>
-          {event?._embedded?.venues[0].name},{" "}
-          {event?._embedded?.venues[0].city.name},{" "}
-          {event?._embedded?.venues[0].state.name}
-        </p>
+  return (
+    <>
+      {event.images && event.images.length > 0 ? (
+        <img className="mt-2" src={event?.images[0]?.url} />
+      ) : (
+        <img src="" alt="No image available" />
+      )}
+      <Favourites
+        setFavourites={setFavourites}
+        favourites={favourites}
+        event={event}
+      />
+      <p>{event?.name}</p>
+      <p>{dateStr}</p>
+      <p>
+        {event?._embedded?.venues[0].name},{" "}
+        {event?._embedded?.venues[0].city.name},{" "}
+        {event?._embedded?.venues[0].state.name}
+      </p>
 
-        <button onClick={() => setPopoutVisible(true)}>More info</button>
+      <button onClick={() => setPopoutVisible(true)}>More info</button>
 
-        <MoreInfoEvent
-          event={event}
-          trigger={popoutVisible}
-          setTrigger={setPopoutVisible}
-        />
+      <MoreInfoEvent
+        event={event}
+        trigger={popoutVisible}
+        setTrigger={setPopoutVisible}
+      />
 
-        <button className="rounded-full m-5 bg-blue-500" onClick={handleClick}>
-          Purchase
-        </button>
-      </>
-    );
-
+      <button className="rounded-full m-5 bg-blue-500" onClick={handleClick}>
+        Purchase
+      </button>
+    </>
+  );
 }

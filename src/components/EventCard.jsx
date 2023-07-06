a3832081598710ae0939aaea00a9b3c8b9dd87aa
t-s-w <tobby.satyarama@gmail.com>
2023-07-06 12:20:59 +0800

    handle missing state for non-us events

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -2,36 +2,37 @@ import { Link } from "react-router-dom";
 
 export default function EventCard({ event }) {
 
-    const options = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-    };
-    const dateISO = event?.dates.start.dateTime;
-    const dateObj = dateISO ? new Date(dateISO) : null;
-    const dateStr = dateObj ? dateObj.toLocaleString("en-GB", options) : '';
-    const timeStr = dateObj ? dateObj.toLocaleTimeString("en-US", { timeZone: event?._embedded?.venues[0]?.timezone, timeZoneName: "short", hour: "numeric", minute: "numeric" }) : '';
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const dateISO = event?.dates.start.dateTime;
+  const dateObj = dateISO ? new Date(dateISO) : null;
+  const dateStr = dateObj ? dateObj.toLocaleString("en-GB", options) : '';
+  const timeStr = dateObj ? dateObj.toLocaleTimeString("en-US", { timeZone: event?._embedded?.venues[0]?.timezone, timeZoneName: "short", hour: "numeric", minute: "numeric" }) : '';
 
-    return (
-      <>
-        <div className="w-1/3 p-3">
-          <img
-            className="w-full h-40 object-cover"
-            src={event?.images[0].url}
-          />
-          <Link to={`/events/${event?._id}`} className="font-bold">
-            {event?.name}
-          </Link>
-          <p>
-            {event?._embedded.venues[0]?.city?.name},{" "}
-            {event?._embedded.venues[0]?.state?.name}
-          </p>
-          <p>
-            {dateStr} {timeStr}
-          </p>
-        
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div className="w-1/3 p-3">
+        <img
+          className="w-full h-40 object-cover"
+          src={event?.images[0].url}
+        />
+        <Link to={`/events/${event?._id}`} className="font-bold">
+          {event?.name}
+        </Link>
+        <p>
+          {event?._embedded.venues[0]?.city?.name}{event?._embedded.venues[0]?.state ?
+            `, ${event?._embedded.venues[0]?.state?.name}`
+            : null}
+        </p>
+        <p>
+          {dateStr} {timeStr}
+        </p>
+
+      </div>
+    </>
+  );
 }

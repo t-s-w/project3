e1a039bf8169fd83b23aad3edd8d9bee0c9d5764
t-s-w <tobby.satyarama@gmail.com>
2023-07-07 09:27:25 +0800

    adjust not logged in page styling

--- src/components/NotLoggedIn.jsx
+++ src/components/NotLoggedIn.jsx
@@ -1,7 +1,7 @@
 import LoginForm from "./LoginForm"
 
 export default function NotLoggedIn() {
-    return <div className="mb-auto flex flex-col items-center">
+    return <div className="flex flex-col items-center">
         <h1>Not logged in!</h1>
         <h2>Log in now to buy tickets, add events to favourites and see personal recommendations</h2>
         <div className="w-60">
